9f8544b93798d38362b9987f1140b69272964748
quirogak <quirogakill@gmail.com>
2023-01-13 11:07:31 -0500

    Added a basic async function to fetch the data.
    And convert it into JSON.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("jojo")
+const processAPI = (() => {
+
+   const fetchAPI = async () => {
+
+   try {
+   const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b6e2e1fb6cdedd11a6bcd01984a7cc13", {mode: 'cors'})
+   const catchData = await response.json();
+    
+   console.log(catchData)
+
+      } catch (error) {
+        console.log("please put a valid search value")
+      }
+
+   }
+
+   return {fetchAPI}
+
+})();
+
+processAPI.fetchAPI()
