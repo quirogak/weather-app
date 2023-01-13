8a16d00314f55031413d3f28ee18af64f6d47d0e
quirogak <quirogakill@gmail.com>
2023-01-13 11:37:38 -0500

    Processed the data to get only the required one.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
+const DOMElements = (() => {
+
+})();
+
+
 const processAPI = (() => {
 
-   const fetchAPI = async () => {
+   const fetchAPI = async (location) => {
 
+   const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID=b6e2e1fb6cdedd11a6bcd01984a7cc13"
+   
    try {
-   const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b6e2e1fb6cdedd11a6bcd01984a7cc13", {mode: 'cors'})
+   const response = await fetch(weatherURL, {mode: 'cors'})
    const catchData = await response.json();
-    
-   console.log(catchData)
+   return await catchData
 
       } catch (error) {
-        console.log("please put a valid search value")
+        return console.log("please put a valid search value")
       }
 
    }
 
-   return {fetchAPI}
+   const processData = async () => {
+
+    const fetchedData = await fetchAPI("Bogotá")
+    const weatherDetails = await fetchedData.weather[0]
+    const mainDetails = await fetchedData.main
+    const locationName = await fetchedData.name
+    console.log(weatherDetails)
+    console.log(mainDetails)
+    console.log(locationName)
+    
+
+   }
+
+   
+
+   return {processData}
 
 })();
 
-processAPI.fetchAPI()
+processAPI.processData()
+
